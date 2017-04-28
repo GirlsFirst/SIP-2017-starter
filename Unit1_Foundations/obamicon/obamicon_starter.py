badfd36e2f7621d638302a64876a1c684de5bf75
SarahGWC <sarah.judd@girlswhocode.com>
2017-04-28 13:22:48 -0500

    Update obamicon_starter.py

--- Unit1_Foundations/obamicon/obamicon_starter.py
+++ Unit1_Foundations/obamicon/obamicon_starter.py
@@ -9,3 +9,13 @@ yellow = (252, 227, 166)
 # Import image.
 my_image = Image.open("IMAGENAME")
 image_list = my_image.getdata()
+image_list = list(image_list)
+
+#YOUR CODE SHOULD GO HERE
+
+
+# Create a new image using the recolored list. Display and save the image.
+new_image = Image.new("RGB", my_image.size)
+new_image.putdata(recolored)
+new_image.show()
+new_image.save("recolored.jpg", "jpeg")

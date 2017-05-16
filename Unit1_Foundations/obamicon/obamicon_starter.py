3c4aa7157eaae1cd1d45b32abaf300a34af3cee9
SarahGWC <sarah.judd@girlswhocode.com>
2017-05-16 14:00:25 -0400

    Update obamicon_starter.py
    Added more explanation

--- Unit1_Foundations/obamicon/obamicon_starter.py
+++ Unit1_Foundations/obamicon/obamicon_starter.py
@@ -1,17 +1,23 @@
 from PIL import Image
 
-# For recoloring.
+# RGB values for recoloring.
 darkBlue = (0, 51, 76)
 red = (217, 26, 33)
 lightBlue = (112, 150, 158)
 yellow = (252, 227, 166)
 
 # Import image.
-my_image = Image.open("IMAGENAME")
+my_image = Image.open("IMAGENAME") #change IMAGENAME to the path on your computer to the image you're using
 image_list = my_image.getdata()
-image_list = list(image_list)
+image_list = list(image_list) #THis will hold a list of 
 
-#YOUR CODE SHOULD GO HERE
+recolored = [] #list holding the pixel data for the new image.
+
+#check for intensity and color the pixel in recolored based on intensity. 
+#A pixel's "intensity" will be higher if the value of Red Green and Blue added together is higher.
+#Good numbers to start with for intensity - 182, 364, 546 - but your numbers may vary
+#YOUR CODE to loop through the original list of pixels and build a new list based on intensity should go here.
+
 
 
 # Create a new image using the recolored list. Display and save the image.
